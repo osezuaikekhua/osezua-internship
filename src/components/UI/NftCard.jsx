1273feca843d0da2038b4098fe9e1457a38e7d54
Osezua Ikekhua <osezuaikekhua@gmail.com>
2023-09-28 13:51:21 -0500

    Finished all of  Explore section

---
+++ src/components/UI/NftCard.jsx
@@ -0,0 +1,131 @@
+import { Link } from "react-router-dom";
+import Skeleton from "./Skeleton";
+import TimerLogic from "./TimerLogic";
+
+function NftCard({
+  title,
+  authorId,
+  authorImage,
+  expiryDate,
+  nftId,
+  nftImage,
+  price,
+  likes,
+  isLoaded,
+  isSlide,
+  style,
+}) {
+  return (
+    <div
+      className={`col-lg-3 col-md-6 col-sm-6 col-xs-12 ${
+        isSlide ? "keen-slider__slide" : ""
+      }`}
+      style={style}
+    >
+      <div className="nft__item">
+        <div className="author_list_pp">
+          {isLoaded ? (
+            <Link
+              to={`/author/${authorId}`}
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title="Creator: Monica Lucas"
+            >
+              <img className="lazy" src={authorImage} alt="" />
+              <i className="fa fa-check"></i>
+            </Link>
+          ) : (
+            <Link
+              to=""
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title="Creator: Monica Lucas"
+            >
+              <Skeleton
+                width={"3rem"}
+                height={"3rem"}
+                borderRadius={"9999px"}
+              />
+              <i className="fa fa-check"></i>
+            </Link>
+          )}
+        </div>
+        {expiryDate ? (
+          <div className="de_countdown">
+            <TimerLogic time={expiryDate} />
+          </div>
+        ) : null}
+        <div className="nft__item_wrap">
+          <div className="nft__item_extra">
+            <div className="nft__item_buttons">
+              <button>Buy Now</button>
+              <div className="nft__item_share">
+                <h4>Share</h4>
+                <a href="" target="_blank" rel="noreferrer">
+                  <i className="fa fa-facebook fa-lg"></i>
+                </a>
+                <a href="" target="_blank" rel="noreferrer">
+                  <i className="fa fa-twitter fa-lg"></i>
+                </a>
+                <a href="">
+                  <i className="fa fa-envelope fa-lg"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          {isLoaded ? (
+            <Link to={`/item-details/${nftId}`}>
+              <img src={nftImage} className="lazy nft__item_preview" alt="" />
+            </Link>
+          ) : (
+            <Link to="">
+              <Skeleton width={"100%"} height={"14rem"} borderRadius={"12px"} />
+            </Link>
+          )}
+        </div>
+        <div className="nft__item_info">
+          {isLoaded ? (
+            <Link to={`/item-details/${nftId}`}>
+              <h4>{title}</h4>
+            </Link>
+          ) : (
+            <Link to="">
+              <h4>
+                {" "}
+                <Skeleton
+                  width={"6rem"}
+                  height={"1rem"}
+                  borderRadius={"12px"}
+                />
+              </h4>
+            </Link>
+          )}
+
+          <div className="nft__item_price">
+            {isLoaded ? (
+              `${price} ETH`
+            ) : (
+              <Skeleton width={"4rem"} height={"1rem"} borderRadius={"12px"} />
+            )}
+          </div>
+          <div className="nft__item_like">
+            <i className="fa fa-heart"></i>
+            <span>
+              {isLoaded ? (
+                `${likes}`
+              ) : (
+                <Skeleton
+                  width={"2rem"}
+                  height={"1rem"}
+                  borderRadius={"12px"}
+                />
+              )}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NftCard;
--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,78 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import NftCard from "../UI/NftCard";
 
 const ExploreItems = () => {
+  const [responseList, setresponseList] = useState([]);
+  const [renderlimit, setrenderlimit] = useState(8);
+
+  useEffect(() => {
+    fetchApiData(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+    );
+  }, []);
+
+  async function fetchApiData(api) {
+    let response = await axios.get(api);
+    let responseData = await response.data;
+    setresponseList(responseData);
+    console.log(responseData);
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setresponseList((prev) => {
+        return prev.map((item) => item);
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function handleValueChange(event) {
+    const FilterValue = event.target.value;
+    fetchApiData(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${FilterValue}`
+    );
+    console.log(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${FilterValue}`
+    );
+  }
+
+
+
+ 
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+      <select id="filter-items" defaultValue="" onChange={handleValueChange}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
+      {responseList.slice(0, renderlimit).map((item, index) => (
+        <NftCard
           key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+          authorId={item.authorId}
+          authorImage={item.authorImage}
+          expiryDate={item.expiryDate}
+          likes={item.likes}
+          nftId={item.nftId}
+          nftImage={item.nftImage}
+          price={item.price}
+          title={item.title}
+          isLoaded={true}
+          isSlide={true}
           style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        />
       ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+        {renderlimit < responseList.length ? (
+        <div className="col-md-12 text-center">
+          <Link
+            to=""
+            id="loadmore"
+            onClick={() => {
+              setrenderlimit(renderlimit + 4);
+            }}
+            className="btn-main lead"
+          >
+            Load more
+          </Link>
+        </div>
+      ) : null}
     </>
   );
 };
